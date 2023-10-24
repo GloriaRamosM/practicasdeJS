9d559c322f5c20f26434188fad68d4f0d73ad29d
GloriaRamosM <gloriaramosmoran@gmail.com>
2023-10-24 18:34:29 -0300

    Practica del dom

---
+++ js/dom.js
@@ -0,0 +1,8 @@
+// DOM
+const title = document.getElementById("title");
+console.log(title);
+
+const titulos = document.querySelector("h1");
+console.log(titulos);
+
+titulos.innerHTML = "Hooooola mi nuevo mundo ";
