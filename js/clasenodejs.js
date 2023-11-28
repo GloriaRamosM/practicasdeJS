700dd927420352cc98597d4c866ecd060917b228
GloriaRamosM <gloriaramosmoran@gmail.com>
2023-11-28 18:56:04 -0300

    practicando ejercicios sencillos

--- js/clasenodejs.js
+++ js/clasenodejs.js
@@ -1,5 +1,11 @@
-let num1 = parseInt(prompt("Teclea un numero entero"));
+/*let num1 = parseInt(prompt("Teclea un numero entero"));
 let num2 = parseInt(prompt("Teclea un numero entero"));
 let num3 = parseInt(prompt("Teclea un numero entero"));
 let media = (num1 + num2 + num3) / 3;
 alert("La media de los números es " + media);
+*/
+
+let num = parseInt(prompt("Entrar numero entero"));
+let res = num % 2;
+let par = res == 0;
+console.log("¿Es par? " + par);
